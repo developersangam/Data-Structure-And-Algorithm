0c66603e0d46ea033f0db1d1acef4467aaeccc4b
sangam <sangam.singh@antiersolutions.com>
2024-03-05 11:34:11 +0530

    changes

--- src/circularQueue.js
+++ src/circularQueue.js
@@ -1,35 +1,47 @@
 class CircularQueue {
-  constructor() {
+  constructor(capacity) {
     this.head = 0;
     this.tail = 0;
-    this.length = 4;
+    this.capacity = capacity;
+    this.length = 0;
     this.items = {};
   }
 
   enqueue = (element) => {
-    console.log(this.size() === this.length);
-    if (this.size() === this.length) {
-      return "Queue is full";
-    }
-    this.items[this.tail] = element;
-    if (this.tail === this.length) {
-      this.tail = 0;
+    if (this.length != this.capacity) {
+      this.items[this.tail] = element;
+      this.tail = (this.tail + 1) % this.capacity;
+      this.length++;
+      console.log(`Enqueued ${element}`);
     } else {
-      this.tail++;
+      console.log("Queue is full");
     }
   };
 
-  
+  dequeue = () => {
+    if (!this.isEmpty()) {
+      const element = this.items[this.front];
+      delete this.items[this.front];
+      this.front = (this.front + 1) % this.capacity;
+      this.length--;
+      console.log(`${element} is removed`);
+    } else {
+      console.log(`Queue is empty`);
+    }
+  };
 
   size = () => {
-    return this.tail - this.head;
+    return this.length;
   };
 
   isFull = () => {
-    if (this.size() === this.length) {
+    if (this.size() === this.capacity) {
       return true;
+    } else {
+      return false;
     }
   };
+
   isEmpty = () => {
     if (this.size() === 0) {
       return true;
@@ -47,13 +59,18 @@ class CircularQueue {
   };
 }
 
-const cq1 = new CircularQueue();
+const cq1 = new CircularQueue(4);
 
 cq1.enqueue(1);
 cq1.enqueue(1);
 cq1.enqueue(1);
 cq1.enqueue(1);
 cq1.enqueue(1);
+cq1.dequeue();
+cq1.dequeue();
+cq1.dequeue();
+cq1.dequeue();
+cq1.dequeue();
 cq1.enqueue(1);
 console.log(cq1.size());
 console.log(cq1.isFull());
